9264567018145e0dcf61b3f09505060f84685e25
Ted Schelble <ted.h.schelble@gmail.com>
2021-10-22 17:11:34 -0700

    Print detections

--- src/localization_node.py
+++ src/localization_node.py
@@ -5,28 +5,27 @@ import cv2
 import apriltag
 import numpy as np
 from navigation_dev.msg import AprilDetections
-from navigation_dev.msg import Pose 
+from navigation_dev.msg import Pose
 
 
 pose_pub = rospy.Publisher('/current_pose', Pose, queue_size=2)
 
 
 def tag_callback(msg):
-    
+
     pose_msg = Pose()
     pose_msg.header.stamp = msg.header.stamp
-    
-    april_tag_distance = 0.5
-    
-    if msg.detections:
-        
-        pose_msg.pose = msg.detections.matrix[:9]
-        pose_msg.pose[8] -= april_tag_distance        
-        
-    else:
-        
-        pose_msg.pose = []
 
+    april_tag_distance = 0.5
+
+    if msg.detections:
+        print(msg.detections)
+        pose_msg.pose = msg.detections.matrix[:9]
+        pose_msg.pose[8] -= april_tag_distance
+
+    else:
+
+        pose_msg.pose = []
 
     pose_pub.publish(pose_msg)
 
