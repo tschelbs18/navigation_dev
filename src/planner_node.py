5f94d57b69b98c45b9bc1e0df376b533ca1131fe
Ted Schelble <ted.h.schelble@gmail.com>
2021-10-22 19:39:06 -0700

    fix prints

--- src/planner_node.py
+++ src/planner_node.py
@@ -87,10 +87,10 @@ def pose_callback(msg):
 
         r13, r31, x_translation, z_translation = t_matrix.matrix[
             2], t_matrix.matrix[8], t_matrix.matrix[3], t_matrix.matrix[11]
-        print(f'r13: {r13}')
-        print(f'r31: {r31}')
-        print(f'x_trans: {x_translation}')
-        print(f'z_trans: {z_translation}')
+        print('r13: ' + str(r13))
+        print('r31: ' + str(r31))
+        print('x_trans: ' + str(x_translation))
+        print('z_trans: ' + str(z_translation))
         if z_translation > 0.05:
 
             if x_translation < -0.05:
