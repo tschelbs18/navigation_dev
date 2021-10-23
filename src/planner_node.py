220f44d443574269d1ce54486a98689958d20b5c
Ted Schelble <ted.h.schelble@gmail.com>
2021-10-22 17:47:08 -0700

    Add .matrix to tmatrix calls for indexing

--- src/planner_node.py
+++ src/planner_node.py
@@ -7,15 +7,16 @@ import argparse
 import time
 from std_msgs.msg import Float32MultiArray
 from navigation_dev.msg import AprilDetections
-from navigation_dev.msg import Pose 
+from navigation_dev.msg import Pose
 
 
 ctrl_pub = rospy.Publisher('/ctrl_cmd', Float32MultiArray, queue_size=2)
 move = 1.0
 stop = 0.0
 
+
 def parse_args():
-    
+
     parser = argparse.ArgumentParser(description='Inputs for JetBot')
     parser.add_argument("--left_forward_speed", default=0.75)
     parser.add_argument("--right_forward_speed", default=0.65)
@@ -25,78 +26,82 @@ def parse_args():
     print(args)
     return args
 
-def move_forward(distance, left_speed, right_speed, forward_rate = 0.415):
+
+def move_forward(distance, left_speed, right_speed, forward_rate=0.415):
     duration = int(round(distance/forward_rate * 10))
-    
+
     for _ in range(duration):
-        
+
         msg = Float32MultiArray()
         msg.data = [move, -left_speed, -right_speed]
         ctrl_pub.publish(msg)
         time.sleep(0.1)
-    
+
     msg.data = [stop, left_speed, right_speed]
     ctrl_pub.publish(msg)
     time.sleep(1.0)
-        
-    
-def right_turn(turn, left_speed, right_speed, turn_rate = 3.02):
+
+
+def right_turn(turn, left_speed, right_speed, turn_rate=3.02):
     duration = int(round(turn/turn_rate * 10))
-    
+
     for _ in range(duration):
-        
+
         msg = Float32MultiArray()
         msg.data = [move, -left_speed, right_speed]
         ctrl_pub.publish(msg)
         time.sleep(0.1)
-    
+
     msg.data = [stop, left_speed, right_speed]
     ctrl_pub.publish(msg)
     time.sleep(1.0)
-    
-    
-def left_turn(turn, left_speed, right_speed, turn_rate = 1.87):
-    
+
+
+def left_turn(turn, left_speed, right_speed, turn_rate=1.87):
+
     duration = int(round(turn/turn_rate * 10))
-    
+
     for _ in range(duration):
-        
+
         msg = Float32MultiArray()
         msg.data = [move, left_speed, -right_speed]
         ctrl_pub.publish(msg)
         time.sleep(0.1)
-    
+
     msg.data = [stop, left_speed, right_speed]
     ctrl_pub.publish(msg)
     time.sleep(1.0)
-    
+
 
 def pose_callback(msg):
-    
+
     cmd_msg = Float32MultiArray()
     ctrl_pub.publish(cmd_msg)
 
     t_matrix = msg.pose
-    
+
     if not t_matrix:
         right_turn(15, args.left_turn_speed, args.right_turn_speed)
     else:
-        
-        r13, r31, x_translation, z_translation = t_matrix[2], t_matrix[8], t_matrix[3], t_matrix[11] 
+
+        r13, r31, x_translation, z_translation = t_matrix.matrix[
+            2], t_matrix.matrix[8], t_matrix.matrix[3], t_matrix.matrix[11]
         if z_translation > 0.05:
-            
+
             if x_translation < -0.05:
                 right_turn(0.5, args.left_turn_speed, args.right_turn_speed)
             elif x_translation > 0.05:
                 left_turn(0.5, args.left_turn_speed, args.right_turn_speed)
             else:
                 if z_translation >= 0.2:
-                    move_forward(z_translation/5, args.left_forward_speed, args.right_forward_speed)
+                    move_forward(
+                        z_translation/5, args.left_forward_speed, args.right_forward_speed)
                 else:
-                    move_forward(z_translation/2, args.left_forward_speed, args.right_forward_speed)
-                    
+                    move_forward(
+                        z_translation/2, args.left_forward_speed, args.right_forward_speed)
+
         else:
-        
+
             if r13 > 0.05:
                 left_turn(0.5, args.left_turn_speed, args.right_turn_speed)
             elif r13 < 0.05:
@@ -104,15 +109,10 @@ def pose_callback(msg):
             else:
                 print('waypoint 1 reached!')
                 time.sleep(5.0)
-                
-        
-            
-
-    
 
 
 if __name__ == "__main__":
-    
+
     args = parse_args()
     rospy.init_node('planner_node')
     rospy.Subscriber("/current_pose", Pose, pose_callback)
