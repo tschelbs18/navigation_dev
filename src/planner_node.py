20e8ac36c31c42428205790318682fc7701745c1
Ted Schelble <ted.h.schelble@gmail.com>
2021-10-24 16:43:11 -0700

    Testing duration 3 speed

--- src/planner_node.py
+++ src/planner_node.py
@@ -33,7 +33,7 @@ def parse_args():
 
 def move_forward(distance, left_speed, right_speed, forward_rate=0.415):
     duration = int(round(distance/forward_rate * 10))
-    duration = 5
+    duration = 3
 
     for _ in range(duration):
 
