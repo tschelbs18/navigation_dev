92e556cc4dd073a64367339b019732daece00f4e
Ted Schelble <ted.h.schelble@gmail.com>
2021-11-10 16:04:16 -0800

    fix

--- src/localization_node.py
+++ src/localization_node.py
@@ -52,17 +52,17 @@ def mahalanobis_distance(s, f, R, H):
     # Distance calculation
     diff = f-np.matmul(H, s)
     print("f= " + str(f))
-    print("H*s= " + str((np.matmul(H, s)))
+    print("H*s= " + str((np.matmul(H, s))))
     # print("MD Diff: " + str(diff))
     return np.sqrt(np.matmul(np.matmul(np.transpose(diff), np.linalg.inv(R)), (diff)))
 
 
 def update_kalman(s, f, H, sigma, R):
     # Update Kalman filter
-    S=np.matmul(np.matmul(H, sigma), np.transpose(H)) + R
-    K=np.matmul(np.matmul(sigma, np.transpose(H)), np.linalg.inv(S))
-    s=s + np.matmul(K, f-np.matmul(H, s))
-    sigma=np.matmul(np.eye(s.shape[0])-np.matmul(K, H), sigma)
+    S = np.matmul(np.matmul(H, sigma), np.transpose(H)) + R
+    K = np.matmul(np.matmul(sigma, np.transpose(H)), np.linalg.inv(S))
+    s = s + np.matmul(K, f-np.matmul(H, s))
+    sigma = np.matmul(np.eye(s.shape[0])-np.matmul(K, H), sigma)
 
 
 def tag_callback(msg):
@@ -76,71 +76,71 @@ def tag_callback(msg):
     global H
     global idx
 
-    pose_msg=Pose()
-    pose_msg.header.stamp=msg.header.stamp
-    d=[v_t, w_t]
+    pose_msg = Pose()
+    pose_msg.header.stamp = msg.header.stamp
+    d = [v_t, w_t]
 
     if move == 1:
         if idx < len(trajectory):
-            pose_msg.pose.matrix=trajectory[idx]
+            pose_msg.pose.matrix = trajectory[idx]
             pose_pub.publish(pose_msg)
         elif idx == len(trajectory):
-            fi=open("s_matrix.txt", "w")
+            fi = open("s_matrix.txt", "w")
             fi.write(str(s))
             fi.close()
-        move=0
+        move = 0
         idx += 1
         time.sleep(1)
 
     elif msg.ids:
 
-        move=1
+        move = 1
         # Update G with new theta
-        G[0][0]=dt*np.cos(s[2])
-        G[1][0]=dt*np.sin(s[2])
+        G[0][0] = dt*np.cos(s[2])
+        G[1][0] = dt*np.sin(s[2])
         # Predict
-        s=np.matmul(F, s) + np.matmul(G, d)
+        s = np.matmul(F, s) + np.matmul(G, d)
         print("s:" + str(s))
-        sigma=np.matmul(np.matmul(F, sigma), np.transpose(F)) + Q
+        sigma = np.matmul(np.matmul(F, sigma), np.transpose(F)) + Q
 
         for i in msg.detections:
 
-            f=np.array([i.matrix[3]*3.28, i.matrix[11]*3.28])
+            f = np.array([i.matrix[3]*3.28, i.matrix[11]*3.28])
             print(f)
-            num_features=(s.shape[0]-3)/2
-            m_d=[]
+            num_features = (s.shape[0]-3)/2
+            m_d = []
             for j in range(1, num_features+1):
-                H_temp=H
-                H_temp[0][2*j+1]=np.cos(s[2])
-                H_temp[0][2*j+2]=np.sin(s[2])
-                H_temp[1][2*j+1]=-np.sin(s[2])
-                H_temp[1][2*j+2]=np.cos(s[2])
+                H_temp = H
+                H_temp[0][2*j+1] = np.cos(s[2])
+                H_temp[0][2*j+2] = np.sin(s[2])
+                H_temp[1][2*j+1] = -np.sin(s[2])
+                H_temp[1][2*j+2] = np.cos(s[2])
                 m_d.append(mahalanobis_distance(s, f, R, H_temp))
 
             if m_d:
                 print("Min M_D: " + str(np.min(m_d)))
             if m_d and np.min(m_d) < 3.5:
-                corresponding_feature=np.argmin(m_d)+1
-                H_new=H
-                H_new[0][2*corresponding_feature+1]=np.cos(s[2])
-                H_new[0][2*corresponding_feature+2]=np.sin(s[2])
-                H_new[1][2*corresponding_feature+1]=-np.sin(s[2])
-                H_new[1][2*corresponding_feature+2]=np.cos(s[2])
+                corresponding_feature = np.argmin(m_d)+1
+                H_new = H
+                H_new[0][2*corresponding_feature+1] = np.cos(s[2])
+                H_new[0][2*corresponding_feature+2] = np.sin(s[2])
+                H_new[1][2*corresponding_feature+1] = -np.sin(s[2])
+                H_new[1][2*corresponding_feature+2] = np.cos(s[2])
                 # Update
                 update_kalman(s, f, H_new, sigma, R)
             elif m_d and np.min(m_d) >= 3.5 or not m_d:
                 # Add new landmark
                 print("NEW LANDMARK FOUND")
-                s=np.append(s, [s[0] + f[1] * np.cos(s[2])-f[0]*np.sin(s[2]),
-                              s[1] + f[1]*np.sin(s[2])-f[0]*np.cos(s[2])])
-                F=np.eye(s.shape[0])
-                G=np.append(G, [[0, 0], [0, 0]], axis=0)
-                H=np.append(H, [[0, 0], [0, 0]], axis=1)
-                Q=0.1*np.eye(s.shape[0])
-                sigma=np.append(sigma, np.zeros((2, sigma.shape[0])), axis=0)
-                sigma=np.append(sigma, np.zeros((sigma.shape[0], 2)), axis=1)
-                sigma[-1][-1]=0.5
-                sigma[-2][-2]=0.5
+                s = np.append(s, [s[0] + f[1] * np.cos(s[2])-f[0]*np.sin(s[2]),
+                                  s[1] + f[1]*np.sin(s[2])-f[0]*np.cos(s[2])])
+                F = np.eye(s.shape[0])
+                G = np.append(G, [[0, 0], [0, 0]], axis=0)
+                H = np.append(H, [[0, 0], [0, 0]], axis=1)
+                Q = 0.1*np.eye(s.shape[0])
+                sigma = np.append(sigma, np.zeros((2, sigma.shape[0])), axis=0)
+                sigma = np.append(sigma, np.zeros((sigma.shape[0], 2)), axis=1)
+                sigma[-1][-1] = 0.5
+                sigma[-2][-2] = 0.5
     time.sleep(1)
     # time.sleep(0.3)
 
