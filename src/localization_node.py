6472e1794dcfbe0b29142d98ce103fbe275d175c
Ted Schelble <ted.h.schelble@gmail.com>
2021-12-04 23:05:49 -0800

    First test run of HW5 localization and path planning

--- src/localization_node.py
+++ src/localization_node.py
@@ -11,21 +11,20 @@ from navigation_dev.msg import Pose
 
 pose_pub = rospy.Publisher('/current_pose', Pose, queue_size=2)
 
-april_tag_map = [[1.5,0,3*np.pi/2,2],
-                 [4.0,0,3*np.pi/2,1],
-                 [6.5,0,3*np.pi/2,2],
-                 [8.0,1.5,0,1],
-                 [8.0,4.0,0,2],
-                 [8.0,6.5,0,1],
-                 [6.5,8.0,np.pi/2,1],
-                 [4.0,8.0,np.pi/2,2],
-                 [1.5,8.0,np.pi/2,1],
-                 [0,6.5,np.pi,1],
-                 [0,4.0,np.pi,2],
-                 [0,1.5,np.pi,1]]
+april_tag_map = [[1.5, 0, 3*np.pi/2, 2],
+                 [4.0, 0, 3*np.pi/2, 1],
+                 [6.5, 0, 3*np.pi/2, 2],
+                 [8.0, 1.5, 0, 1],
+                 [8.0, 4.0, 0, 2],
+                 [8.0, 6.5, 0, 1],
+                 [6.5, 8.0, np.pi/2, 1],
+                 [4.0, 8.0, np.pi/2, 2],
+                 [1.5, 8.0, np.pi/2, 1],
+                 [0, 6.5, np.pi, 1],
+                 [0, 4.0, np.pi, 2],
+                 [0, 1.5, np.pi, 1]]
 
-robot_pos = [0,0,np.pi/2]
-
+robot_pos = [1, 1, 0]
 
 
 def tag_callback(msg):
@@ -46,41 +45,47 @@ def tag_callback(msg):
         # Get position of robot from april tags
         april_tag = [robot_pos[0] + 3.28084*matrix[11]*np.cos(robot_pos[2]) + 3.28084*matrix[3]*np.sin(robot_pos[2]),
                      robot_pos[1] + 3.28084*matrix[11]*np.sin(robot_pos[2]) + 3.28084*matrix[3]*np.cos(robot_pos[2])]
-        
+
         april_tag_min_distance = 1000
         april_tag_min_index = -1
-        for j,v in enumerate(april_tag_map):
-            april_tag_distance = np.sqrt((v[0]-april_tag[0])**2 + (v[1]-april_tag[1])**2)
+        for j, v in enumerate(april_tag_map):
+            april_tag_distance = np.sqrt(
+                (v[0]-april_tag[0])**2 + (v[1]-april_tag[1])**2)
             if april_tag_distance < april_tag_min_distance:
                 april_tag_min_distance = april_tag_distance
                 april_tag_min_index = j
-        
+
         if april_tag_min_distance < 0.7 and april_tag_map[j][3] == msg.ids[x_argmin]:
-            
+
             rotation_matrix = matrix[:3] + matrix[4:7] + matrix[8:11]
             rotation_matrix = np.reshape(np.array(rotation_matrix), (3, 3))
             rotation_matrix_transpose = np.transpose(rotation_matrix)
-            
+
             translation_vector = -1 * \
-                np.array([matrix[3]] + [matrix[7]] + [matrix[11]+camera_distance])
-    
-            camera_pos = np.matmul(rotation_matrix_transpose, translation_vector)
+                np.array([matrix[3]] + [matrix[7]] +
+                         [matrix[11]+camera_distance])
+
+            camera_pos = np.matmul(
+                rotation_matrix_transpose, translation_vector)
 
             # Format the matrix so that right is positive x, up is positive y (irrelevant), straight distance between robot and april tag is z
             frame_fix = np.reshape(
                 np.array([-1, 0, 0, 0, -1, 0, 0, 0, -1]), (3, 3))
             camera_pos = np.matmul(frame_fix, camera_pos)
-            
-            robot_pos[0] = april_tag_map[april_tag_min_index][0] - camera_pos[2]*np.cos(april_tag_map[april_tag_min_index][2]) - camera_pos[0]*np.sin(april_tag_map[april_tag_min_index][2])
-            robot_pos[1] = april_tag_map[april_tag_min_index][1] - camera_pos[2]*np.sin(april_tag_map[april_tag_min_index][2]) + camera_pos[0]*np.cos(april_tag_map[april_tag_min_index][2])
+
+            robot_pos[0] = april_tag_map[april_tag_min_index][0] - camera_pos[2]*np.cos(
+                april_tag_map[april_tag_min_index][2]) - camera_pos[0]*np.sin(april_tag_map[april_tag_min_index][2])
+            robot_pos[1] = april_tag_map[april_tag_min_index][1] - camera_pos[2]*np.sin(
+                april_tag_map[april_tag_min_index][2]) + camera_pos[0]*np.cos(april_tag_map[april_tag_min_index][2])
 
             # Retrive orientation where 0 is looking at april tag, positive is looking to the left of april tag
-            robot_pos[2] = april_tag_map[april_tag_min_index][2] * matrix[2] * (np.pi/2)
-    
+            robot_pos[2] = april_tag_map[april_tag_min_index][2] * \
+                matrix[2] * (np.pi/2)
+
             # Return position, orientation, and april tag id
             pose_msg.pose.matrix = robot_pos
             print("Robot Position {}".format(robot_pos))
-            
+
         else:
             print("I might be lost")
 
--- src/planner_node.py
+++ src/planner_node.py
@@ -12,83 +12,36 @@ from navigation_dev.msg import AprilDetections
 from navigation_dev.msg import Pose
 
 
-# Code for ground truth of walls and obstacles and functions
 # Euclidean Distance
 def e_dist(p1, p2):
     return ((p2[1] - p1[1])**2 + (p2[0] - p1[0])**2)**0.5
 
 
-# Find the closest point in a set of points relative to a given point
-def get_closest_pt(p1, point_set):
-    min_dist = 10000000000000000
-    for p2 in point_set:
-        dist_check = e_dist(p1, p2)
-        if dist_check < min_dist:
-            min_dist = dist_check
-            closest = p2
-    return closest
-
-
-# Check if a point is a voronoi point, equidistant from the obstacle and the boundary
-def v_check(p, obstacles, boundaries):
-    if abs(e_dist(p, get_closest_pt(p, obstacles)) - e_dist(p, get_closest_pt(p, boundaries))) <= 0.11:
-        return True
-    else:
-        return False
-
-
-# Initialize Walls of boundary points
-l_wall = [[0.0, y / 10.0] for y in range(0, 81)]
-r_wall = [[8.0, y / 10.0] for y in range(0, 81)]
-t_wall = [[x / 10.0, 8.0] for x in range(0, 81)]
-b_wall = [[x / 10.0, 0.0] for x in range(0, 81)]
-
-# Initialize Walls of obstacle points
-l_o_wall = [[3.0, y / 10.0] for y in range(35, 46)]
-r_o_wall = [[5.0, y / 10.0] for y in range(35, 46)]
-t_o_wall = [[x / 10.0, 4.5] for x in range(30, 51)]
-b_o_wall = [[x / 10.0, 3.5] for x in range(30, 51)]
-obstacle_walls = l_o_wall + r_o_wall + t_o_wall + b_o_wall
-walls = l_wall + r_wall + t_wall + b_wall
-
-# Build out a set of voronoi points
-v_pts = []
-for y in range(10, 71):
-    for x in range(10, 21):
-        if v_check([x / 10.0, y / 10.0], obstacle_walls, walls):
-            v_pts.append([x / 10.0, y / 10.0])
-    for x in range(60, 71):
-        if v_check([x / 10.0, y / 10.0], obstacle_walls, walls):
-            v_pts.append([x / 10.0, y / 10.0])
-
-for x in range(20, 61):
-    for y in range(50, 71):
-        if v_check([x / 10.0, y / 10.0], obstacle_walls, walls):
-            v_pts.append([x / 10.0, y / 10.0])
-    for y in range(10, 31):
-        if v_check([x / 10.0, y / 10.0], obstacle_walls, walls):
-            v_pts.append([x / 10.0, y / 10.0])
-
-print(v_pts)
-
-# Initialize publisher, map of april tags, and waypoints
+# Initialize publisher and waypoints
 ctrl_pub = rospy.Publisher('/ctrl_cmd', Float32MultiArray, queue_size=2)
 move = 0.0
 stop = 1.0
-april_tag_map = {
-    0: [6.5, 8.0, "down"],
-    1: [4.0, 8.0, "down"],
-    2: [1.5, 8.0, "down"],
-    3: [0.0, 6.5, "right"],
-    4: [0.0, 4.0, "right"],
-    5: [0.0, 1.5, "right"],
-    6: [1.5, 0.0, "up"],
-    7: [4.0, 0.0, "up"],
-    8: [6.5, 0.0, "up"],
-    9: [8.0, 1.5, "left"],
-    10: [8.0, 4.0, "left"],
-    11: [8.0, 6.5, "left"]}
-waypoints = [[4.0, 6.2], [1.5, 4.0]]
+start = [1, 1]
+robot_width = .4583  # feet
+robot_length = .4167  # feet
+c_size = [6, 6]
+
+# Theoretically traverse the space to plan path
+waypoints = []
+position = start
+finish = [x1 + x2 for x1, x2 in zip(start, c_size)]
+while position != finish and position[1] <= finish[1]:
+    x, y = position
+    # Turn right
+    if x == start[0]:
+        waypoints.append([finish[0], y])
+        waypoints.append([finish[0], y + 0.25])
+    elif x == finish[0]:
+        waypoints.append([start[0], y])
+        waypoints.append([start[0], y + 0.25])
+    position = waypoints[-1]
+# Remove unnecessary turn at the end
+waypoints.pop()
 
 
 def parse_args():
@@ -145,35 +98,13 @@ def left_turn(duration, left_speed, right_speed):
     time.sleep(1.0)
 
 
-# Get the real position of the robot in the [0,0] - [8,8] coordinate plane
-def get_real_position(x, y, orientation, tag_x, tag_y, tag_facing):
-    # Based on the tags relative orientation, find robot's x, y, and orientation in radians
-    if tag_facing == "down":
-        real_x = tag_x - x
-        real_y = tag_y - y
-        real_orientation = (3.1415 / 2) + orientation
-    elif tag_facing == "right":
-        real_x = tag_x + y
-        real_y = tag_y - x
-        real_orientation = 3.1415 + orientation
-    elif tag_facing == "up":
-        real_x = tag_x + x
-        real_y = tag_y + y
-        real_orientation = (3.1415 * 3 / 2) + orientation
-    elif tag_facing == "left":
-        real_x = tag_x - y
-        real_y = tag_y + x
-        real_orientation = 0.0 + orientation
-    return real_x, real_y, real_orientation
-
-
 def pose_callback(msg):
 
     # Assign global variables that keep track of our progress
     t_matrix = msg.pose
     global waypoints
 
-    # If waypoints all found, do nothing
+    # If waypoints all found, do nothing (or... find untouched points and cover them)
     if len(waypoints) == 0:
         pass
     # Turn right until you find an april tag (given layout of HW4, turning right should be better)
@@ -182,33 +113,16 @@ def pose_callback(msg):
         # right_turn(1, args.left_turn_speed, args.right_turn_speed)
     else:
         # Once found, determine position relative to april tag & convert to feet
-        x, z, y, orientation = t_matrix.matrix[0] * 3.28084, t_matrix.matrix[1] * \
-            3.28084, t_matrix.matrix[2] * 3.28084, t_matrix.matrix[3]
-        tag_id = t_matrix.matrix[4]
+        x, y, orientation, tag_id = t_matrix.matrix[0], t_matrix.matrix[1], t_matrix.matrix[2], t_matrix.matrix[3]
         print("Tag ID: " + str(tag_id))
-
-        # Get ground truth of tag location
-        tag_x = april_tag_map[tag_id][0]
-        tag_y = april_tag_map[tag_id][1]
-        tag_facing = april_tag_map[tag_id][2]
-
-        # Using the tag, calculate our real x & y & orientation based on tag perception
-        x, y, orientation = get_real_position(
-            x, y, orientation, tag_x, tag_y, tag_facing)
         print('x: ' + str(x))
         print('y: ' + str(y))
         print('orientation: ' + str(orientation))
 
-        # Find next closest voronoi point that is closer to the destination than the robot
-        dest_dist = e_dist([x, y], waypoints[0])
-        test_pts = [p for p in v_pts if e_dist(
-            p, waypoints[0]) < dest_dist - 0.5 or p == waypoints[0]]
-        closest_v_pt = get_closest_pt([x, y], test_pts)
-        print("Closest path point: " + str(closest_v_pt))
-
-        # Given our v point destination, determine distance needed to move or turn
-        move_x = closest_v_pt[0] - x
-        move_y = closest_v_pt[1] - y
+        # Given our next waypoint destination, determine distance needed to move or turn (EDIT ALL BELOW)
+        destination = waypoints[0]
+        move_x = destination[0] - x
+        move_y = destination[1] - y
         print("Move x: " + str(move_x) + ", Move y: " + str(move_y))
         needed_turn = math.atan2(move_y, move_x) - orientation
         # Ensure we don't unncessarily turn in one direction
@@ -218,13 +132,20 @@ def pose_callback(msg):
             needed_turn = 2 * math.pi + needed_turn
         print("Needed turn: " + str(needed_turn))
 
-        # Determine actions needed to reach nearest voronoi point (if more than .1 feet away)
-        if e_dist(closest_v_pt, [x, y]) > .1:
-            if abs(needed_turn) < 0.25:
+        # Determine actions needed to reach next waypoint (if more than .1 feet away)
+        if e_dist(destination, [x, y]) > .1:
+            # Big move for more than 0.5 feet away
+            if abs(needed_turn) < 0.20 and e_dist(destination, [x, y]) > .5:
                 # Move forward towards next point
                 print("Moving forward")
                 move_forward(
-                    3, args.left_forward_speed, args.right_forward_speed)
+                    5, args.left_forward_speed, args.right_forward_speed)
+            # Smaller move for less than 6 inches away
+            elif abs(needed_turn) < 0.20:
+                # Move forward towards next point
+                print("Moving forward")
+                move_forward(
+                    2, args.left_forward_speed, args.right_forward_speed)
 
             elif needed_turn > 0:
                 # Align to next point by turning left
@@ -239,18 +160,10 @@ def pose_callback(msg):
                 print("Unaccounted situation! Help! x = {}, y = {}, orientation = {}".format(
                     x, y, orientation))
         else:
-            # Check if voronoi point was destination point
-            if closest_v_pt == waypoints[0]:
-                print("Waypoint reached, current position is x = {}, y = {} with an error of {}".format(
-                    x, y, np.sqrt(x**2 + y**2)))
-                waypoints.pop(0)
-                time.sleep(5.0)
-            # Handle a potential getting stuck where the closest v point is not a destination
-            # And we still need to keep moving
-            else:
-                print("Moving forward slow")
-                move_forward(1, args.left_forward_speed,
-                             args.right_forward_speed)
+            # Arrived at destination
+            print("Waypoint reached, current position is x = {}, y = {} with an error of {}".format(
+                x, y, np.sqrt(x**2 + y**2)))
+            waypoints.pop(0)
 
 
 if __name__ == "__main__":
