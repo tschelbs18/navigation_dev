465f28c3135d0e70920da4dddd8c36c9e86c9704
Ted Schelble <ted.h.schelble@gmail.com>
2021-12-05 18:21:01 -0800

    Increase speed

--- src/planner_node.py
+++ src/planner_node.py
@@ -138,13 +138,13 @@ def pose_callback(msg):
                 # Move forward towards next point
                 print("Moving forward")
                 move_forward(
-                    1, args.left_forward_speed, args.right_forward_speed)
+                    3, args.left_forward_speed, args.right_forward_speed)
             # Smaller move for less than 6 inches away
             elif abs(needed_turn) < 0.20:
                 # Move forward towards next point
                 print("Moving forward")
                 move_forward(
-                    1, args.left_forward_speed, args.right_forward_speed)
+                    2, args.left_forward_speed, args.right_forward_speed)
 
             elif needed_turn > 0:
                 # Align to next point by turning left
